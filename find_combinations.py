cb7fe88e658a2835764a6674f814dbfab74e4b7c
David Lukes <dafydd.lukes@gmail.com>
2016-08-01 18:23:27 +0200

    construct flight index while searching
    - the index reduces the search space
    - also polished interface

--- find_combinations.py
+++ find_combinations.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 
+import sys
 import csv
 import json
+import argparse as ap
 import fileinput as finp
 
-from datetime import datetime as dt
+from datetime import timedelta, datetime as dt
+from collections import defaultdict
+from sortedcontainers import SortedListWithKey
+from operator import itemgetter
 
 DFMT = "%Y-%m-%dT%H:%M:%S"
-MINT = 60*60  # seconds
-MAXT = 4*60*60  # seconds
+MINT = timedelta(hours=1)
+MAXT = timedelta(hours=4)
 
 
 def parse_dates(record):
@@ -17,34 +22,115 @@ def parse_dates(record):
     return record
 
 
-def main(files=None):
-    """Entry point.
+def jsonify(x):
+    if isinstance(x, dt):
+        return dt.strftime(x, DFMT)
+    elif isinstance(x, SortedListWithKey):
+        return list(x)
+    else:
+        raise RuntimeError("Unable to jsonify {!r}.".format(x))
 
-    :param files: Files to process.
-    :type files: iterable or None. If None, STDIN and/or files in
-                 ``sys.argv[1:]`` are processed.
+
+def is_abab(itin, record):
+    """Will appending ``record`` to ``itin`` result in (A->B), (_->_), (A->B)?
+
     """
+    itin = itin["itin"]
+    if len(itin) < 2:
+        return False
+    penult = itin[-2]
+    return (penult["source"] == record["source"]
+            and penult["destination"] == record["destination"])
+
+
+def find_itins(flights, mint, maxt, cleanup_cycles=False):
+    """Connect flights into multi-flight itineraries.
+
+    :param flights: Iterable of flight records.
+    :param mint: Minimum flight transfer time.
+    :param maxt: Maximum flight transfer time.
+    :param cleanup_cycles: Whether to remove cyclic references (for JSON
+        output).
+
+    """
+    # sorting the records simplifies subsequent code quite a bit because
+    # itineraries can then only grow by appending new flights, and never by
+    # prepending
+    flights = sorted(flights, key=itemgetter("arrival"))
+    by_dst = defaultdict(lambda: SortedListWithKey(key=lambda flight: flight["arrival"]))
     itins = []
+    for flight in flights:
+        parse_dates(flight)
+        itin = dict(itin=[flight], valid=False, maximal=True)
+        flight["ends"] = [itin]
+        itins.append(itin)
+        by_dst[flight["destination"]].append(flight)
+        rdep = flight["departure"]
+        # this is *not* brute force search, the only candidates we throw away
+        # are those that would result in an (A->B), (B->A), (A->B) itinerary
+        for arrival in by_dst[flight["source"]].irange_key(rdep - maxt, rdep - mint):
+            for itin in arrival["ends"]:
+                if not is_abab(itin, flight):
+                    new = dict(itin=itin["itin"][:] + [flight], valid=True, maximal=True)
+                    itins.append(new)
+                    flight["ends"].append(new)
+                    itin["maximal"] = False
+    if cleanup_cycles:
+        for dst in by_dst.values():
+            for record in dst:
+                del record["ends"]
+    return itins
+
+
+def output(itins, sub_itins=False, fmt="debug"):
+    if fmt == "debug":
+        print(json.dumps(itins, indent=2, default=jsonify))
+        return
+
+    def should_output(itin):
+        if sub_itins:
+            return itin["valid"]
+        else:
+            return itin["valid"] and itin["maximal"]
+
+    filtered_itins = enumerate(i for i in itins if should_output(i))
+
+    for i, itin in filtered_itins:
+        if fmt == "human":
+            print("ITINERARY #{}".format(i + 1))
+            for rec in itin["itin"]:
+                print("  Flight {}: From {} (at {}) to {} (at {})".format(
+                    rec["flight_number"], rec["source"], rec["departure"],
+                    rec["destination"], rec["arrival"]))
+        elif fmt == "flights":
+            print(",".join(rec["flight_number"] for rec in itin["itin"]))
+        elif fmt == "airports":
+            print(",".join(rec["source"] for rec in itin["itin"]), end=",")
+            print(itin["itin"][-1]["destination"])
+        else:
+            raise RuntimeError("Unknown output format {!r}.".format(fmt))
+
+
+def parse_argv(argv):
+    prs = ap.ArgumentParser(description="Find itineraries in flight list.",
+                            formatter_class=ap.ArgumentDefaultsHelpFormatter)
+    prs.add_argument("files", metavar="FILE", nargs="?", default="-",
+                     help="Input file; '-' for STDIN.")
+    prs.add_argument("-s", "--sub-itins", action="store_true",
+                     help="Output sub-itineraries as separate entries.")
+    prs.add_argument("-f", "--fmt", help="Output format.", default="flights",
+                     choices="debug human flights airports".split())
+    return prs.parse_args(argv)
+
+
+def main(argv):
+    args = parse_argv(argv)
     # NOTE: data will get corrupted if CSV fields contain newlines; to avoid
     # this, only accept input from a file and ``open(..., newline="")``
-    rdr = csv.DictReader(finp.input(files))
-    for record in rdr:
-        parse_dates(record)
-        rsrc, rdst = record["source"], record["destination"]
-        rdep, rarr = record["departure"], record["arrival"]
-        for itin in itins:
-            isrc, idst = itin[0]["source"], itin[-1]["destination"]
-            idep, iarr = itin[0]["departure"], itin[-1]["arrival"]
-            start_diff = (idep - rarr).total_seconds()
-            end_diff = (rdep - iarr).total_seconds()
-            if rdst == isrc and start_diff >= MINT and start_diff <= MAXT:
-                itin.insert(0, record)
-            elif idst == rsrc and end_diff >= MINT and end_diff <= MAXT:
-                itin.append(record)
-        itins.append([record])
-    itins = [itin for itin in itins if len(itin) > 1]
-    print(json.dumps(itins, indent=2, default=lambda x: dt.strftime(x, DFMT)))
+    rdr = csv.DictReader(finp.input(args.files))
+    itins = find_itins(rdr, MINT, MAXT, args.fmt == "debug")
+    output(itins, args.sub_itins, args.fmt)
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
